d78b1fe0961d143579ddfbc92ba7e7565a33557f
Javier Pulido <javipulido15@gmail.com>
2026-07-01 02:43:57 +0100

    Super usuario creado

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,15 @@ mkdir -p /app/data
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput --clear
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+    print('Superusuario admin creado.')
+else:
+    print('Superusuario admin ya existe.')
+"
 
 exec gunicorn Ecosistema_Personal.wsgi:application \
     --bind 0.0.0.0:8888 \
